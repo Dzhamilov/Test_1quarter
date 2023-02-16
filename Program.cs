4fb19f0f35303b063867f6fc9024b28a2bbe8f56
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2023-02-16 08:28:31 +0300

    считывание сколько элементов подходящие условию

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿string[] arr = new string[5]{"55","Moscow","yes","Dushanbe","GB"};
 Console.Write(string.Join("; ", arr));
 Console.WriteLine();
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3) count++;
+}
